f7bb3e2095ee9a72688bd679a2e236e0924ed8ff
wangshaokang <1321525769@qq.com>
2020-03-30 18:07:35 +0800

    日志---2020/03/30

--- src/main/java/com/example/demo/controller/OkHttpController.java
+++ src/main/java/com/example/demo/controller/OkHttpController.java
@@ -1,13 +1,21 @@
 package com.example.demo.controller;
 
+import com.google.gson.Gson;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OkHttpController {
 
+    private static Logger logger = LoggerFactory.getLogger(OkHttpController.class);
+
+    Gson gson = new Gson();
+
     @GetMapping
     public String hello() {
+        logger.debug(gson.toJson("132132"));
         return "hello";
     }
 
